d4e5afc6d0601be1a10ce588fbf8cbd515a839c9
Minea Stefan Teodor <mineateo@gmail.com>
2025-02-12 18:02:51 +0200

    Delete config.sh

--- config.sh
+++
@@ -1,4 +0,0 @@
-SERVER_NAME="fifoserver"
-SERVER_PATH="/home/teominea/Desktop/facultate/ITBI/proiect/$SERVER_NAME"
-REPLY_FORMAT="server-reply-"
-REPLY_PATH="/home/teominea/Desktop/facultate/ITBI/proiect/$REPLY_FORMAT"
